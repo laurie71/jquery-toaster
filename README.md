4f51147e63e82bdbcf7a8ff32d57a13dbf22905f
Laurie Harper <laurie@holoweb.net>
2011-05-02 02:24:03 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,37 @@
+# jQuery Toaster
+
+`jquery-toaster` is a jQuery plugin that shows stacked transient notification messages. 
+
+<img alt="Sample image showing 'toast' notifications" src="./docs/sample.png">
+     
+## Features
+
+* Progressive enhancement allows toasts to be read when JavaScript is disabled
+* Support for *info*, *notice*, *warning* and *error* notices
+* Straight-forward API: `$(selector).toast()` to get started
+* Easily tailored through options and events
+* Fully customizable
+
+## Demos
+
+## Documentation
+
+To get started, just use `$(selector).toast()` -- whatever `selector` matches will be shown as a toast message. You can also create toasts without existing markup in the page by passing the message as the jQuery selector: `$('<p>A toast message.</p>').toast()`. To close a toast message, use `$(selector).toast('close')`.
+
+The `toast()` plugin can optionally be given a hash of options to control its behaviour. For example, to make a toast sticky (so it doesn't fade out automatically), set the `sticky` option like so: `$(selector).toast({ sticky: true })`.
+
+See the [project wiki][] for documentation on all the supported options and events.
+
+## Download
+
+Download the [latest version][] of the plugin, or clone the [GitHub project][] to get the full source. Feel free to fork and improve -- pull requests are appreciated!
+
+## License
+
+The jQuery Toaster is available under the MIT license, the same as jQuery itself. See [License.md][] for the full terms and conditions.
+
+
+[project wiki] (https://github.com/jquery-toaser/wiki)
+[latest version] (https://github.com/jquery-toaser/...)
+[GitHub project] (https://github.com/jquery-toaser)
+[License.md] (./License.md)
---
+++ toaster.js
@@ -0,0 +1,217 @@
+(function($) {
+    // --------------------------------------------------------------------
+    // Base toaster impl
+
+    var defaults = { 
+        toaster: {
+            position: { top: 0, right: 0 }
+        },
+        toast: { 
+            width: 200,
+            
+            selector: '.toaster',
+            
+            type: 'info',
+            text: '',
+            closeText: 'Close',
+            
+            sticky: false,
+            duration: 6000,
+            
+            close: null
+        }
+    }
+
+
+    function Toaster(options) {
+        this.options = $.extend({}, defaults.toast, options);
+        this.toasts = [];
+    }
+    
+    Toaster.prototype.add = function(toast) {
+        this.toasts.push(toast);
+        return toast;
+    }
+    
+    Toaster.prototype.remove = function(toast) {
+        var idx = this.toasts.indexOf(toast);
+        if (idx >= 0) return this.toasts.splice(idx, 1);
+        return null;
+    }
+    
+    Toaster.prototype.clear = function() {
+        $.each(this.toasts, function(i, t) {
+            t.close();
+        });
+    }
+    
+    
+    function Toast(toaster, options) {
+        this.options = $.extend({}, defaults.toast, options);
+        this.toaster = toaster;
+        this.toaster.add(this)
+            .fadeIn();
+    }
+    
+    Toast.prototype.close = function() {
+        this.toaster.remove(this)
+            .fadeOut();
+    }
+    
+    Toast.prototype.fadeIn = function() {} // xxx todo
+    Toast.prototype.fadeOut = function() {} // xxx todo
+    
+    // --------------------------------------------------------------------
+    // jQuery toaster plugins
+    
+    function cssTRBL($el, property, values) {
+        var dirs = ['top','right','bottom','left'],
+            prefix = property ? property + '-' : '',
+            props = {};
+        
+        if (typeof(values) == 'string') {
+            values = [values, values, values, values];
+        } else if (values.length == 2) {
+            values = [values[0], values[1], values[0], values[1]];
+        }
+        
+        $.each(dirs, function(i, d) {
+            if (values[i] !== undefined) {
+                props[prefix + dirs[i]] = values[i];
+            }
+        });
+
+        $el.css(props);
+    }
+
+    (function($) {
+        // ========
+        // TODO: add events
+        // ========
+        $.fn.toaster = toaster;
+    
+        var methods = {
+            init: function(options) {
+                var defaults = toaster.defaults,
+                    opts = $.extend({}, defaults, options),
+                    $this = this.first();
+                    
+                opts.position = opts.position 
+                    || defaults.position 
+                    || { top: 0, right: 0 };
+                    
+                $this.first()
+                    .data('toaster', new Toaster(opts))
+                    .addClass('toaster')
+                    .css('position', 'absolute');
+                    
+                cssTRBL($this, '', [
+                    opts.position.top,
+                    opts.position.right,
+                    opts.position.bottom,
+                    opts.position.left
+                ]);
+
+                cssTRBL($this, 'margin', '1em');
+                cssTRBL($this, 'padding', '1em');
+                
+                return this;
+            },
+            
+            clear: function() { $(this).data('toaster').clear() }
+        };
+    
+        function toaster(method) {
+            if ((! method) || typeof(method) === 'object') {
+                return methods.init.apply(this, arguments);
+            } else if (methods[method]) {
+                return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+            } else  {
+              $.error('Method ' +  method + ' does not exist on jQuery.toaster');
+            }
+        };
+    
+        toaster.methods = methods;
+        toaster.defaults = defaults.toaster;
+    }($));
+
+    (function() {
+        // ========
+        // TODO: add events
+        // ========
+        $.fn.toast = toast;
+    
+        var methods = {
+            init: function(options) {
+                var defaults = toast.defaults,
+                    opts = $.extend({}, defaults, options),
+                    sel = opts.selector || defaults.selector,
+                    type = opts.type = opts.type || defaults.type || 'info',
+                    closeTxt = opts.closeText || 'Close Notice',
+                    toaster = $(sel).first();
+console.log('toast type: '+type);
+
+                if (! toaster.length) {
+                    toaster = $('<div>').toaster({ cls: sel }).appendTo('body');
+                }
+
+                this.each(function(i, el) {
+                    var $this = $('<div>')
+                        .data('toast', opts)
+                        .addClass('toast toast-'+type)
+                        .appendTo(toaster)
+                        .append('<a href="#close" title="'+closeTxt+'">X</a>')
+                        // .append('<a href="#close">X</a>')
+                        .append(el);
+                    
+                    if (opts.width) {
+                        $this.css('width', opts.width);
+                    }
+                    
+                    $('>a', $this)
+                        .click(function(e) {
+                            e.preventDefault();
+                            $this.toast('close');
+                        });
+                        
+                    if (! opts.sticky) {
+                        setTimeout(function() {
+                            $this.toast('close');
+                        }, opts.duration);
+                    }
+                });
+                
+                return this;
+            },
+            
+            close: function() {
+                var opts = this.data('toast'),
+                    $this = this;
+                    
+                $this.animate({ opacity: '0' }, 600, function() {
+                    $this.animate({  height: '0', margin: 0, padding: 0 }, 600, function() {
+                        $this.remove();
+                        if (typeof(opts.close) == 'function') {
+                            opts.close();
+                        }
+                    })
+                });
+            }
+        };    
+
+        function toast(method) {
+            if ((! method) || typeof(method) === 'object') {
+                return methods.init.apply(this, arguments);
+            } else if (methods[method]) {
+                return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+            } else  {
+              $.error('Method ' +  method + ' does not exist on jQuery.toast');
+            }
+        };
+
+        toast.methods = methods;
+        toast.defaults = defaults.toast;
+    }());
+    
+    // --------------------------------------------------------------------
+}($));
